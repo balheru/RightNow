de04c2274ba07bf495454f80f86c82c538fc4b7c
balheru <valeriu.ioan.potra@gmail.com>
2018-06-19 14:25:38 -0400

    This is a simple connection to My Mysql DB on aws

---
+++ main.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "rds-mysql-10mval.cn7hthmsur2l.us-east-2.rds.amazonaws.com",
+  user: "val",
+  password: "val"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
